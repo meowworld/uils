8081f5417a7a9911b5a4d994340990fae16fae80
meow_world <39392518+meowworld@users.noreply.github.com>
2019-11-16 20:54:51 +0800

    Update GeneralType.java
    泛型的使用方法

--- reflect/GeneralType.java
+++ reflect/GeneralType.java
@@ -27,5 +27,38 @@ public class Meow<T> {
 
 
   //todo:这种泛型方法一般用来从某个地方get后 reture 根据传入的 Class<T> 转换后返回
+    
+    public <T> T test(Class<T> clazz) throws Exception {
+        T t = clazz.newInstance();
+        //todo 要想返回 T 泛型，方法入参必须有 T 才能使用
+        return t;
+    }
+
+    public <T> List<T> test2(Class<T> clazz) throws Exception {
+        List<T> list = new ArrayList<>();
+        T t = clazz.newInstance();
+        list.add(t);
+        return list;
+    }
+
+    public <T> List<T> test(Collection<T> collection) throws Exception {
+        List<T> list = new ArrayList<>();
+        return list;
+    }
+
+    public void test4(Object obj) {
+        if (obj instanceof List) {
+            List list = (List) obj;
+            for (Object o : list) {
+                //xxx
+            }
+        }
+        if (obj instanceof Map) {
+            //xxx
+        }
+        if (obj instanceof String) {
+            //xxx
+        }
+    }
 
 }
